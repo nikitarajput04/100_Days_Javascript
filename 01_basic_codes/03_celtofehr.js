66acd23474d94c464cb7ef5f1f5ec2f938dfec50
Nikita Rajput <nikitarajput0701@gmail.com>
2025-04-22 23:05:09 +0530

    initial_commit03

---
+++ 01_basic_codes/03_celtofehr.js
@@ -0,0 +1,8 @@
+function celsiusToFahrenheit(celsius) {
+    const fahrenheit = (celsius * 9/5) + 32;
+    return fahrenheit;
+  }
+  const celsiusInput = 56;
+  const result = celsiusToFahrenheit(celsiusInput);
+  console.log(`${celsiusInput}°C is ${result}°F`);
+  
