8935bd4e3354a8b2f7a0d971486965f9e55a2b8c
Nikita Rajput <nikitarajput0701@gmail.com>
2025-05-31 04:00:59 +0530

    initial_commit38

---
+++ 01_basic_codes/37_substring.js
@@ -0,0 +1,7 @@
+const message = "Hello, my self nikikta !";
+
+if (message.includes("self")) {
+  console.log("The word 'self ' is in the message.");
+} else {
+  console.log("The word 'self' is NOT in the message.");
+}
