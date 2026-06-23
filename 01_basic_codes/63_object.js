ba171e7be11a499e09de787c287106734508409f
Nikita Rajput <nikitarajput0701@gmail.com>
2026-06-23 16:25:39 +0530

    initialCommit67

---
+++ 01_basic_codes/63_object.js
@@ -0,0 +1,22 @@
+const product = {
+    title: "Parker Jotter Standard CT Ball Pen (Black)",
+    rating: 4,
+    reviews: 7002,
+    price: 270,
+    mrp: 285,
+    discount: "5% off"
+};
+
+console.log(product);
+
+const profile = {
+    username: "shradhakhapra",
+    name: "Shradha Khapra",
+    posts: 195,
+    followers: "569K",
+    following: 4,
+    profession: "Entrepreneur",
+    bio: "Apna College | Ex-Microsoft, DRDO"
+};
+
+console.log(profile);
